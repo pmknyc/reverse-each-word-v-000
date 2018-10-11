75d83820fbcd5dbaad9235de66dc4604db9f61fd
Paula McKinley <pmknyc@optimum.net>
2018-10-11 23:32:27 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
 #reverse_each_word
 # call string sentence argument
 # split into array of words,
-# iterate on array to reverse each word 
-# join array back into string of reversed words and 
+# iterate on array to reverse each word
+# collect words into array
+# join array back into string of reversed words and
 # returns that same sentence with each word reversed in place.
 
 def reverse_each_word(string)
